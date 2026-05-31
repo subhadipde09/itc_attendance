cb64cc82a85f5011315011b3222cd4953e685742
subhadipde09 <subhadipdeece22l65@gmail.com>
2026-06-01 00:05:38 +0530

    Done

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,10 +1,12 @@
 const app = require('./app');
 const connectDB = require('./config/db');
 const env = require('./config/env');
+require('dotenv').config();
+const PORT = process.env.PORT || 5000;
 
 connectDB()
   .then(() => {
-    app.listen(env.port, () => console.log(`Backend running on port ${env.port}`));
+    app.listen(PORT, () => console.log(`Backend running on port ${PORT}`));
   })
   .catch((error) => {
     console.error('Failed to start backend:', error);
